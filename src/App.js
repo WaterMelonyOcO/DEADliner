959af478a6373e7b0086efcb2591d2e4d3eaabaf
sonia13marker <soniamarker13@gmail.com>
2023-05-14 22:43:45 +0300

    очистка

--- src/App.js
+++ src/App.js
@@ -1,16 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 import './style.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Hello world</h1>
-      </header>
     </div>
   );
 }
