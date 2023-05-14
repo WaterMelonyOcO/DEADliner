a17d0483190deebf70985e8e2af69015cc43e390
sonia13marker <soniamarker13@gmail.com>
2023-05-15 00:03:34 +0300

    левое меню: начало
    остановилась на ошибке с link to. надо продумать компоненты кнопки, поместить их в ссылки, стилизовать.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import './style.scss';
 import { MainPage } from './pages/MainPage/MainPage';
+import { Layout } from './components/Layout/Layout';
+import { LeftMenu } from './components/LeftMenu/LeftMenu';
 
-export function App() {
+function App() {
   return (<>
     <BrowserRouter>
     <Layout>
         <Routes>
           <Route path='/' element={<MainPage />}></Route>
-        </Routes>
+          </Routes>
       </Layout>
       </BrowserRouter>
+<LeftMenu/>
   </>
 
   );
 }
+export default App;
--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -4,17 +4,18 @@ import main_icon from '../../images/main_page.svg';
 import archive_icon from '../../images/archive.svg';
 import info_icon from '../../images/info.svg';
 import error_icon from '../../images/error.svg';
+import { Link } from 'react-router-dom';
 
 export function LeftMenu () {
     return(<>
         <div className="head">
-            <img src={logo} alt="logo" />
+            <img src={logo} alt="logo" className="head__logo"/>
 
             <ul className="head__ul">
-                <li><img src={main_icon} alt="main_icon"/>Главная</li>
-                <li><img src={archive_icon} alt="arch_icon"/>Архив</li>
-                <li><img src={info_icon} alt="info_icon"/>О разработчиках</li>
-                <li><img src={error_icon} alt="error_icon"/>Сообщить об ошибке</li>
+                <li><Link to=""><img src={main_icon} alt="main_icon"/>Главная</Link></li>
+                <li><Link to=""><img src={archive_icon} alt="arch_icon"/>Архив</Link></li>
+                <li><Link to=""><img src={info_icon} alt="info_icon"/>О разработчиках</Link></li>
+                <li><Link to=""><img src={error_icon} alt="error_icon"/>Сообщить об ошибке</Link></li>
             </ul>
             <p className="head__version_p">
             version 0.0.1
