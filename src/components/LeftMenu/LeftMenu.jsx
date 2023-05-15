98004a167e7e3f552c7efcc07b0d285b5631533f
sonia13marker <soniamarker13@gmail.com>
2023-05-15 19:22:50 +0300

    левое меню
    закончила левое меню с переключением по страницам. нужно потом заменить тег <a> на <Link to>.

--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -14,51 +14,57 @@ import { useState } from 'react';
 export function LeftMenu () {
 
     /* для красных иконок */
-    const [isSelected, setIsSelected] = useState(false);
-    const handleClick = () => {
-        setIsSelected(!isSelected);
-      }
+    const [selectedLink, setSelectedLink] = useState(0);
+    const links = [
+        {
+          name: "Главная",
+          selectedIcon: main_icon_red,
+          defaultIcon: main_icon
+        },
+        {
+          name: "Архив",
+          selectedIcon: archive_icon_red,
+          defaultIcon: archive_icon,
+        },
+        {
+          name: "О разработчиках",
+          selectedIcon: info_icon_red,
+          defaultIcon: info_icon,
+        },
+        {
+          name: "Сообщить об ошибке",
+          selectedIcon: error_icon_red,
+          defaultIcon: error_icon,
+        },
+      ];
+
+      const handleClick = (linkIndex) => {
+        setSelectedLink(linkIndex);
+      };
 
     return(<>
         <div className="head">
             <img src={logo} alt="logo" className="head__logo"/>
 
             <nav className='head__nav'>
-                <a onClick={handleClick} href='#' className='head__link'>
-                    <span className='head__span head__span_active'>
-                        <img src={ isSelected ? main_icon_red : main_icon} alt="icon" className='head__span__img'/>
-                        Главная
-                    </span>
-                </a>
-
-                <a onClick={handleClick} href='#' className='head__link'>
-                    <span className='head__span'>
-                        <img src={isSelected ? archive_icon_red : archive_icon} alt="icon" className='head__span__img'/>
-                        Архив
-                    </span>
-                </a>
-
-                <a onClick={handleClick} href='#' className='head__link'>
-                    <span className='head__span'>
-                        <img src={isSelected ? info_icon_red : info_icon} alt="icon" className='head__span__img'/>
-                        О разработчиках
-                    </span>
-                </a>
-
-                <a onClick={handleClick} href='#' className='head__link'>
-                    <span className='head__span'>
-                        <img src={isSelected ? error_icon_red : error_icon} alt="icon" className='head__span__img'/>
-                        Сообщить об ошибке
-                    </span>
-                </a>
+                {links.map((link, index) => (
+                    <a
+                    key={index}
+                    className={selectedLink === index ? "head__link_selected" : "head__link"}
+                    onClick={() => handleClick(index)}
+                    >
+                    <img
+                        src={
+                        selectedLink === index ? link.selectedIcon : link.defaultIcon
+                        }
+                        alt={link.name}
+                        className='head__link__img'
+                    />
+                    {link.name}
+                    </a>
+                ))}
             </nav>
 
-            {/* <ul className="head__ul">
-                <li><Link to=""><img src={main_icon} alt="main_icon"/>Главная</Link></li>
-                <li><Link to=""><img src={archive_icon} alt="arch_icon"/>Архив</Link></li>
-                <li><Link to=""><img src={info_icon} alt="info_icon"/>О разработчиках</Link></li>
-                <li><Link to=""><img src={error_icon} alt="error_icon"/>Сообщить об ошибке</Link></li>
-            </ul> */}
             <p className="head__version__p">
             version 0.0.1
             </p>
