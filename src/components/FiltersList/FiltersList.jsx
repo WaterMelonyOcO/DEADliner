d213e21a4d036d37792b5a5cb403a89a4aa2c591
sonia13marker <soniamarker13@gmail.com>
2023-05-15 23:20:01 +0300

    добавление инпута фильтров
    добавление инпута фильтров со стилями и отступами

--- src/components/FiltersList/FiltersList.jsx
+++ src/components/FiltersList/FiltersList.jsx
@@ -2,8 +2,8 @@ import style from './style.scss';
 import { useEffect, useState } from 'react';
 import arrow_icon from '../../images/arrow_icon.svg';
 
-export function TagsList ({placeholder, options}) {
-    // console.log(options[3])
+export function FiltersList ({placeholder, options}) {
+
     const [showMenu, setShowMenu] = useState(false);
     const [selectedValue, setSelectedValue] = useState(null);
 
@@ -28,15 +28,8 @@ export function TagsList ({placeholder, options}) {
         return placeholder;
       };
 
-      /* поменять функцию на "Сделать новый тег" */
-      function createNewTag() {
-        console.log("hello, im 3");
-      }
       const onItemClick = (option) => {
 
-        if (option === options[3]) {
-            createNewTag();
-        } else 
         setSelectedValue(option);
       };
       const isSelected = (option) => {
@@ -45,26 +38,23 @@ export function TagsList ({placeholder, options}) {
       return selectedValue.value === option.value;
     };
 
-    return(
-    <div className="dropdown__container">
-        <div onClick={handleInputClick} className="dropdown__input">
-            <div>{getDisplay()}</div>
+    return(<div className="dropdown__container">
+    <div onClick={handleInputClick} className="dropdown__input">
+        <div>{getDisplay()}</div>
+        <div>
             <div>
-                <div>
-                    <img src={arrow_icon} alt="icon"/>
-                </div>
+                <img src={arrow_icon} alt="icon"/>
             </div>
         </div>
-        {showMenu && (
-        <div className='dropdown__menu'>
-            {options.map((option) => (
-                <div key={option.value} className={`dropdown__item ${isSelected(option) && "selected"}`} onClick={() => onItemClick(option)}>
-                    {option.label}
-                    <img src={option.image}/>
-                    
-                    </div>
-            ))}
-        </div>
-        )}
-  </div>)
+    </div>
+    {showMenu && (
+    <div className='dropdown__menu'>
+        {options.map((option) => (
+            <div key={option.value} className={`dropdown__item ${isSelected(option) && "selected"}`} onClick={() => onItemClick(option)}>
+                {option.label}
+                </div>
+        ))}
+    </div>
+    )}
+</div>)
 }
--- src/components/TopMenu/TopMenu.jsx
+++ src/components/TopMenu/TopMenu.jsx
@@ -4,6 +4,7 @@ import study_icon from '../../images/study_icon.png';
 import home_icon from '../../images/home_icon.png';
 import new_tag from '../../images/new_tag_icon.png';
 import { TagsList } from '../TagsList/TagsList';
+import { FiltersList } from '../FiltersList/FiltersList';
 
 export function TopMenu () {
     const options = [
@@ -12,9 +13,16 @@ export function TopMenu () {
         { value: 'дом', label: 'Дом', image: home_icon},
         { value: 'новый тег', label: 'Новый тег', image: new_tag}
       ];
+      const options2 = [
+        { value: 'Время на исходе', label: 'Время на исходе'},
+        { value: 'Время еще есть', label: 'Время еще есть'},
+        { value: 'Недавно добавленные', label: 'Недавно добавленные'},
+        { value: 'Давно добавленные', label: 'Давно добавленные'}
+      ]
     return <div className='search'>
             <input type='search' name="search_input" className='input__search' placeholder='Поиск'/>
             <TagsList placeholder="Теги" options={options}/>
+            <FiltersList placeholder="Фильтры" options={options2}/>
 
     </div>
 }
