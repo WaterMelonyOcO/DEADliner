d63e3cc50dbb748566a9a800bbbf6e24f770ba10
SoNya Marker <soniamarker13@gmail.com>
2023-05-21 20:56:03 +0300

    исправление расположения меню и контента

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
 import './style.scss';
 import { MainPage } from './pages/MainPage/MainPage';
 import { Layout } from './components/Layout/Layout';
+import { GoToArchive } from './components/GoToArchive/GoToArchive';
+import { LeftMenu } from './components/LeftMenu/LeftMenu';
+import { TopMenu } from './components/TopMenu/TopMenu';
+import { CreateTaskBtn } from './components/CreateTaskBtn/CreateTaskBtn';
 
 function App() {
   return (<div className='body'>
     <BrowserRouter>
-    <Layout>
+    <div className='body__content'>
+    {/* <Layout> */}
+      <div className='body__content-left'>
+      <LeftMenu /> 
+      </div>
+
+      <div className='body__content-top'>
+        <TopMenu />
+      </div>
+
+      <div className='body__content-center'>
         <Routes>
+          
           <Route path='/' element={<MainPage />}></Route>
+
           </Routes>
-      </Layout>
+      </div>
+          
+      {/* </Layout> */}
+      <CreateTaskBtn />
+      </div>
       </BrowserRouter>
   </div>
 
--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -2,7 +2,7 @@ import { GoToArchive } from '../../components/GoToArchive/GoToArchive';
 import style from './style.scss';
 
 export function MainPage () {
-    return (<div>
+    return (<div className='main'>
         <h1>hello</h1>
         <GoToArchive />
     </div>)
