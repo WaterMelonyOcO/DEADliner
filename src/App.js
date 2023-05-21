f7c62aef7617bf8c25b0d3fe2f828d332996b8f5
SoNya Marker <soniamarker13@gmail.com>
2023-05-21 21:04:26 +0300

    чистка

--- src/App.js
+++ src/App.js
@@ -1,37 +1,35 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './style.scss';
 import { MainPage } from './pages/MainPage/MainPage';
-import { Layout } from './components/Layout/Layout';
-import { GoToArchive } from './components/GoToArchive/GoToArchive';
 import { LeftMenu } from './components/LeftMenu/LeftMenu';
 import { TopMenu } from './components/TopMenu/TopMenu';
 import { CreateTaskBtn } from './components/CreateTaskBtn/CreateTaskBtn';
 
 function App() {
   return (<div className='body'>
+
     <BrowserRouter>
     <div className='body__content'>
-    {/* <Layout> */}
-      <div className='body__content-left'>
-      <LeftMenu /> 
-      </div>
 
-      <div className='body__content-top'>
-        <TopMenu />
-      </div>
+        <div className='body__content-left'>
+        <LeftMenu /> 
+        </div>
 
-      <div className='body__content-center'>
-        <Routes>
-          
-          <Route path='/' element={<MainPage />}></Route>
+        <div className='body__content-top'>
+          <TopMenu />
+        </div>
 
-          </Routes>
-      </div>
-          
-      {/* </Layout> */}
+        <div className='body__content-center'>
+          <Routes>
+            
+            <Route path='/' element={<MainPage />}></Route>
+
+            </Routes>
+        </div>
+
       <CreateTaskBtn />
-      </div>
-      </BrowserRouter>
+    </div>
+    </BrowserRouter>
   </div>
 
   );
