af1a4f30f34220393944f748602fdc0748a18c81
SoNya Marker <soniamarker13@gmail.com>
2023-05-22 21:04:42 +0300

    переиспользуемый компонент

--- src/components/CreateNewTaskWindow/CreateNewTaskWindow.jsx
+++ src/components/CreateNewTaskWindow/CreateNewTaskWindow.jsx
@@ -1,57 +1,20 @@
+/* eslint-disable no-undef */
 import './style.scss';
-import { RedBtn } from '../RedBtn/RedBtn';
-import plus_icon from '../../images/new_tag_icon.png';
 
-export function CreateNewTaskWindow ({active, setActive}) {
-// function fuck () {
-//     setActive(false);
-//     console.log("CLOSE PLS");
-// }
+export function CreateNewTaskWindow ({active, setActive, children, title}) {
+
     return (
         <div className={active ? "modal__overlay active" : "modal__overlay"} >
             <div className="modal__dialog">
                 <div className="modal__content">
                 <div className="modal__header">
-                    <h1 className='modal__header__title'>Создание задания</h1>
-                    {/* <button className="modal__close" onClick={fuck}>
-                    </button> */}
+                    <h1 className='modal__header__title'>{title}</h1>
+                    <button className="modal__close" onClick={() => console.log("hhhh")}>
+                    </button>
                 </div>
-                <form className="modal__body" id="createTask">
-                    <span className='modal__body__wrapper'>
-                    <label className='modal__body__label' for="name"> Название</label>
-                        <input type='text' className='modal__body__input' name="name" placeholder='Введи название задания'/>
-                    </span>
-                    <span className='modal__body__wrapper'>
-                    <label className='modal__body__label' for="description">Описание</label>
-                    <textarea className='modal__body__textarea' name="description" placeholder="Введи описание задания (не более 300 символов)"></textarea>
-                    </span>
-
-                    <span className='modal__body__wrapper'>
-                    <label className='modal__body__label' for="deadline">Дата завершения</label>
-                    <input type="date" className='modal__body__input small__input' name="deadline"/>
-                    </span>
-
-{/*тут кастомное вложение */}
-                    <span className='modal__body__wrapper file__wrapper'>
-                    <p className='modal__body__label'>Вложение</p>
-                    <label className='modal__body__input small__input file__body'>Добавить файл</label>
-                        <input type='file' name="files" className='file'/>
                     
-                    </span>
-
-
-                    {/* 
-                     Добавить тег + 
-должен находиться раскрывающийся список
-                    <label className='modal__body__label' for="name"> Название</label>
-                    <input type='text' className='modal__body__input' name="name"/> */}
-            <span className='modal__body wrapper_btns'>
-                    <RedBtn text="Отменить задание" outline="outline" onClick={() => setActive(false)}/>
-                    <RedBtn text="Создать задание"/>
-            </span>      
-
-                </form>
                 </div>
+                {children}
             </div>
     </div>
     )
--- src/components/CreateTaskBtn/CreateTaskBtn.jsx
+++ src/components/CreateTaskBtn/CreateTaskBtn.jsx
@@ -2,6 +2,7 @@ import style from './style.scss';
 import icon_plus from '../../images/button-plus.png';
 import { CreateNewTaskWindow } from '../CreateNewTaskWindow/CreateNewTaskWindow';
 import { useState } from 'react';
+import { RedBtn } from '../RedBtn/RedBtn';
 
 export function CreateTaskBtn() {
     /*тут функция onClick должна вызывать модальное окно 
@@ -10,9 +11,35 @@ export function CreateTaskBtn() {
     
     return(<button className='wrapper__btn' onClick={() => setModalActive(true)}>
          
-            <CreateNewTaskWindow active={modalActive} setActive={setModalActive}>
-                {/* <h1>Заголовок модального окна</h1>
-                <p>Текст модального окна</p> */}
+            <CreateNewTaskWindow active={modalActive} setActive={setModalActive} title="Создание задания">
+            <form className="modal__body" id="createTask">
+                    <span className='modal__body__wrapper'>
+                    <label className='modal__body__label' for="name"> Название</label>
+                        <input type='text' className='modal__body__input' name="name" placeholder='Введи название задания'/>
+                    </span>
+                    <span className='modal__body__wrapper'>
+                    <label className='modal__body__label' for="description">Описание</label>
+                    <textarea className='modal__body__textarea' name="description" placeholder="Введи описание задания (не более 300 символов)"></textarea>
+                    </span>
+
+                    <span className='modal__body__wrapper'>
+                    <label className='modal__body__label' for="deadline">Дата завершения</label>
+                    <input type="date" className='modal__body__input small__input' name="deadline"/>
+                    </span>
+
+{/*тут кастомное вложение */}
+                    <span className='modal__body__wrapper file__wrapper'>
+                    <p className='modal__body__label'>Вложение</p>
+                    <label className='modal__body__input small__input file__body'>Добавить файл</label>
+                        <input type='file' name="files" className='file'/>
+                    
+                    </span>
+                    <span className='modal__body wrapper_btns'>
+                    <RedBtn text="Отменить задание" outline="outline" onClick={() => setModalActive(false)}/>
+                    <RedBtn text="Создать задание"/>
+            </span>      
+
+                </form>
             </CreateNewTaskWindow>
  
         <img src={icon_plus} alt="icon"/>
